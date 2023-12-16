5b717fc2747c00943e585d7b7181ab3d14d73c70
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-12-16 19:38:32 +0200

    fixed ui bugs in cart page menu page registartion page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,13 +19,13 @@ const LoginPage = () => {
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/* BOX */}
-      <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+      <div className=" h-[90%] shadow-2xl rounded-md flex flex-col justify-center lg:flex-row   lg:w-[60%] 2xl:w-1/2">
         {/* IMAGE CONTAINER */}
-        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+        <div className="lg:relative lg:h-[100%] lg:w-[50%] lg:block  hidden ">
           <Image src="/loginBg.png" alt="" fill className="object-cover" />
         </div>
         {/* FORM CONTAINER */}
-        <div className="p-10 flex flex-col gap-8 md:w-1/2">
+        <div className="p-10 flex flex-col gap-8 lg:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
           <button
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -20,19 +20,13 @@ const MenuPage = async () => {
         <Link
           href={`/menu/${category?.slug}`}
           key={category.id}
-          className="w-full h-1/3 bg-cover p-8 md:h-1/2"
+          className="w-full h-1/3 bg-cover p-8 md:h-[53%]"
           style={{ backgroundImage: `url(${category.img})` }}
         >
           <div className={`text-${category.color} w-1/2`}>
-            <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
-            <p className="text-sm my-8">{category.desc}</p>
-            <button
-              className={`hidden 2xl:block bg-${category.color} text-${
-                category.color === "black" ? "white" : "red-500"
-              } py-2 px-4 rounded-md`}
-            >
-              Explore
-            </button>
+            <h1 className="uppercase font-bold text-[16px] sm:text-3xl leading-normal">{category.title}</h1>
+            <p className="text-[12px] leading-normal sm:text-sm ">{category.desc}</p>
+            
           </div>
         </Link>
       ))}
