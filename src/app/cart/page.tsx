99779c9cbbf27657d60f03463935f4a9659b54c0
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-12-16 19:38:32 +0200

    fixed ui bugs in cart page menu page registartion page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -33,9 +33,9 @@ const CartPage = () => {
   };
 
   return (
-    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
+    <div className="   flex flex-col text-red-500 lg:flex-row">
       {/* PRODUCTS CONTAINER */}
-      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-y-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
         {/* SINGLE ITEM */}
         {products.map((item) => (
           <div
@@ -43,11 +43,11 @@ const CartPage = () => {
             key={`${item.id}-${item.optionTitle}`}
           >
             {item.img && (
-              <Image src={item.img} alt="" width={100} height={100} />
+              <Image className="hidden sm:block" src={item.img} alt="" width={100} height={100} />
             )}
             <div className="">
-              <h1 className="uppercase text-xl font-bold">
-                {item.title} x{item.quantity}
+              <h1 className="uppercase text-sm sm:text-xl font-bold">
+               <span>{item.title} </span>x<span>{item.quantity}</span>
               </h1>
               <span>{item.optionTitle}</span>
             </div>
@@ -62,7 +62,7 @@ const CartPage = () => {
         ))}
       </div>
       {/* PAYMENT CONTAINER */}
-      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-auto lg:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
         <div className="flex justify-between">
           <span className="">Subtotal ({totalItems} items)</span>
           <span className="">${Number(totalPrice).toFixed(2)}</span>
